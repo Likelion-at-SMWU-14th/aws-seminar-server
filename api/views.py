17c235065f698bf095de8d352d7404f38cc0e784
bluebomin <bluebomin@sookmyung.ac.kr>
2026-07-07 00:08:57 +0900

    Fix : JsonResponse ensure ascii

--- api/views.py
+++ api/views.py
@@ -6,15 +6,21 @@ def home(request):
 
 
 def health_check(request):
-    return JsonResponse({
-        "status": "ok",
-        "message": "server is running"
-    })
+    return JsonResponse(
+        {
+            "status": "ok",
+            "message": "server is running"
+        },
+        json_dumps_params={"ensure_ascii": False}
+    )
 
 
 def api_test(request):
-    return JsonResponse({
-        "title": "AWS 배포 세미나",
-        "message": "Django server response success!",
-        "method": request.method
-    })
+    return JsonResponse(
+        {
+            "title": "AWS 배포 세미나",
+            "message": "Django server response success!",
+            "method": request.method
+        },
+        json_dumps_params={"ensure_ascii": False}
+    )
